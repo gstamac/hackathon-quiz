752073930ce16679bfa4ddb69ef37e8a626c0315
Matej Ajster <matej.ajster@global.id>
2020-12-21 17:31:24 +0100

    add dynamic quiz form

--- src/components/global/dialogs/game_quiz_dialog/game_quiz_dialog.tsx
+++ src/components/global/dialogs/game_quiz_dialog/game_quiz_dialog.tsx
@@ -1,5 +1,6 @@
-import { TextInput, Checkbox } from 'globalid-react-ui'
-import React from 'react'
+import { Button } from '@material-ui/core'
+import { TextInput, Checkbox, PrimaryButton, ButtonState, updateMultipleValidators, partiallyUpdateValueObject } from 'globalid-react-ui'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from 'RootType'
 import { setGameQuizFormState } from '../../../../store/ui_slice'
@@ -9,13 +10,33 @@ import { FormDialog } from '../form_dialog'
 import { useStyles } from './styles'
 
 const quiz: CustomFieldDefinition<{}> = {
-  question0: [],
+  question0: ['is_required'],
+  option0_0: [],
   option0_1: [],
   option0_2: [],
-  option0_3: [],
+  check0_0: [],
   check0_1: [],
   check0_2: [],
-  check0_3: [],
+}
+
+interface QuizFormQuestionSectionProps {
+  questionNumber: number
+}
+
+interface QuizFormAnswersSectionProps {
+  questionNumber: number
+  answerNumber: number
+  numberOfAnswers: number
+}
+
+const range = (length: number): number[] => Array.from({ length }, (_, i: number) => i)
+
+interface QuestionState {
+  numberOfAnswers: number
+}
+
+interface QuestionsState {
+  [key: number]: QuestionState | undefined
 }
 
 export const GameQuizDialog: React.FC = () => {
@@ -23,50 +44,89 @@ export const GameQuizDialog: React.FC = () => {
   const dispatch = useDispatch()
   const classes = useStyles()
 
-  const getQuizInputs = () => [...new Array(1)].map((_, i) => <>
-    <TextInput
-      className={i !== 0 ? classes.question: ''}
-      fieldId={`question${i}`}
-      label={`${i+1}. Question`}
-      fullWidth
-    />
-    <div style={{display:'flex'}}>
-      <TextInput
-        className={classes.option}
-        fieldId={`option${i}_1`}
-        label={'1. Option'}
-      />
+  const formId: string = 'quiz-form'
 
-      <Checkbox
-        fieldId={`check${i}_1`}
-        checkedIcon={<CheckboxOnIcon/>}
-        icon={<CheckboxOffIcon/>}
-      />
-    </div>    <div style={{display:'flex'}}>
+  console.log('quiz', quiz)
+
+  const [numberOfQuestions, setNumberOfQuestions] = useState<number>(1)
+
+  const addQuestion = () => {
+    const nextQuestionNumber: number = numberOfQuestions
+
+    const validators = range(3).reduce((o: object, answerNumber: number) => {
+      const optionKey: string = `option${nextQuestionNumber}_${answerNumber}`
+      const checkboxKey: string = `check${nextQuestionNumber}_${answerNumber}`
+    
+      partiallyUpdateValueObject(formId, optionKey, {
+        failed_validators: [],
+        has_changed: false,
+        messages: [],
+        value: '',
+      })
+      partiallyUpdateValueObject(formId, checkboxKey, {
+        failed_validators: [],
+        has_changed: false,
+        messages: [],
+        value: false,
+      })
+
+      return {
+        ...o,
+        [optionKey]: [],
+        [checkboxKey]: [],
+      }
+    }, {})
+
+    const questionKey: string = `question${nextQuestionNumber}`
+
+    updateMultipleValidators(formId, {
+      ...validators,
+      [questionKey]: ['is_required'],
+    })
+
+    partiallyUpdateValueObject(formId, questionKey, {
+      failed_validators: [],
+      has_changed: false,
+      messages: [],
+      value: '',
+    })
+    
+    setNumberOfQuestions((prev: number) => prev + 1)
+  }
+
+  const getQuizFormQuestionSection = ({
+    questionNumber,
+  }: QuizFormQuestionSectionProps): JSX.Element => {
+
+    const getAnswers = () => range(3).map((i: number) => {
+
+      console.log('fieldId text', `option${questionNumber}_${i}`)
+      console.log('fieldId check', `check${questionNumber}_${i}`)
+
+      return <div style={{display:'flex'}} key={`${questionNumber}_${i}`}>
+        <Checkbox
+          fieldId={`check${questionNumber}_${i}`}
+          checkedIcon={<CheckboxOnIcon/>}
+          icon={<CheckboxOffIcon/>}
+        />
+        <TextInput
+          className={classes.option}
+          fieldId={`option${questionNumber}_${i}`}
+          label={`${i + 1}. Option`}
+        />
+      </div>
+    })
+
+    return <>
       <TextInput
-        className={classes.option}
-        fieldId={`option${i}_2`}
-        label={'2. Option'}
+        className={questionNumber !== 0 ? classes.question: ''}
+        fieldId={`question${questionNumber}`}
+        label={`${questionNumber+1}. Question`}
+        fullWidth
       />
-      <Checkbox
-        fieldId={`check${i}_2`}
-        checkedIcon={<CheckboxOnIcon/>}
-        icon={<CheckboxOffIcon/>}
-      />
-    </div>
-    <div style={{display:'flex'}}>
-      <TextInput
-        className={classes.option}
-        fieldId={`option${i}_3`}
-        label={'3. Option'}
-      />
-      <Checkbox
-        fieldId={`check${i}_3`}
-        checkedIcon={<CheckboxOnIcon/>}
-        icon={<CheckboxOffIcon/>}
-      />
-    </div>
-  </>)
+      {getAnswers()}
+    </>
+  }
 
   return <FormDialog
     className={classes.quiz}
@@ -74,12 +134,16 @@ export const GameQuizDialog: React.FC = () => {
     formSubtitle={'Setup your question and answers for the hackathon quiz!'}
     onFormSubmit={async () => {}}
     open={formOpen}
-    formId={'quiz-form'}
-    fieldId='description'
+    formId={formId}
     fieldDefinition={quiz}
     onExit={() => dispatch(setGameQuizFormState(false))}
     fullScreenOnMobile={true}
   >
-    {getQuizInputs()}
+    {range(numberOfQuestions).map((questionNumber: number) => getQuizFormQuestionSection({ questionNumber }))}
+    <Button
+      onClick={() => addQuestion()}
+    >
+      Add question
+    </Button>
   </FormDialog>
 }
